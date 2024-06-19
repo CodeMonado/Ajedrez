c2726c7fd7a4f7d3b61769d2e5e50d6a62dc3333
CodeMonado <reyRobOT13@gmail.com>
2024-06-19 15:05:55 -0600

    Añadido la libreria WPF

--- Ajedrez/Program.cs
+++ Ajedrez/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine();
+﻿using SixLabors.ImageSharp;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Ajedrez {
+    class Main : Window {
+        public Main() {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
+ 
