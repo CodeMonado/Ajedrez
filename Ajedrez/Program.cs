64adbdf9ae7cf2f54b025d800e4a1bc76a12bf39
CodeMonado <reyRobOT13@gmail.com>
2024-06-20 12:43:07 -0600

    Añadidos archivos .xaml.cs e implementado de manera completa la libreria WPF

--- Ajedrez/Program.cs
+++ Ajedrez/Program.cs
@@ -1,20 +1,9 @@
-﻿using SixLabors.ImageSharp;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
+﻿using System;
 
 namespace Ajedrez {
-    class Main : Window {
-        public Main() {
-            Console.WriteLine("Hello, world!");
+    public static class Program {
+        public static void MiFuncion() {
+            Console.WriteLine("MiFuncion ha sido llamada.");
         }
     }
 }
- 
